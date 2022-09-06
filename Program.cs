58436a1648028b473694631482dcb0ebcd5b2f76
Joseph K Justus <justuskasyoki57@gmail.com>
2022-09-06 11:05:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace browser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = "https://www.google.com";
+            var psi = new System.Diagnostics.ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = uri;
+            System.Diagnostics.Process.Start(psi);
+        }
+        public static void getdetails(string accountNo,string memberNo)
+        {
+
+        }
+    }
+}
